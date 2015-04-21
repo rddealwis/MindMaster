097587c221a6827c66f03e8cdd95ccec55885183
chintakawijetunga <chintaka.wijetunga@yahoo.com>
2015-04-21 22:22:01 +0530

    Multiple level access, score and level implemented

--- MindMaster/src/com/infinity/mindmaster/MemoryTiles.java
+++ MindMaster/src/com/infinity/mindmaster/MemoryTiles.java
@@ -17,6 +17,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mindmaster.R;
@@ -25,13 +26,14 @@ import com.example.mindmaster.R;
 
 public class MemoryTiles extends ActionBarActivity {
 
-	// GameLogic gl = new GameLogic();
 	public ArrayList randomNumbers = new ArrayList();
 	public ArrayList userInput = new ArrayList();
 	public int gameLevel = 4;
 	public int checkCount = 0;
 	public boolean test = false;
 	public boolean correct = true;
+	public int score=0;
+	public int level=0;
 	GridView gridview = null;
 
 	@Override
@@ -40,38 +42,11 @@ public class MemoryTiles extends ActionBarActivity {
 		setContentView(R.layout.activity_memory_tiles);
 
 		gridview = (GridView) findViewById(R.id.gridView_Tile);
-		//gridview.setAdapter(new LoadTileIcons(this));
 		
-		gridview.setOnItemClickListener(new OnItemClickListener() {
-						
-			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-				if (test) 
-				{
-					if(CheckUserInput(position))
-					{						
-						//GamePlay();
-					}
-				}
-				else
-				{					
-					Toast.makeText(MemoryTiles.this, position + " blinked", Toast.LENGTH_SHORT).show();
-					}
-				Log.d("---------", "clicked "+position);
-			}
-		});
 	}
 
-	public void startPlay(View v)
-	{
-		Log.d("----","eeeeee");
-		
-		int time = 1;
-		for(int i=0; i<5;i++)
-		{
-			color(v, i+1,time);
-			time=time+2;
-		}
-	}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -99,104 +74,74 @@ public class MemoryTiles extends ActionBarActivity {
 		//color();
 	}
 
+	public void startPlay(View v)
+	{		
+		playGame();
+	}
+	
+	public void playGame(){
+		final TextView levelTxt = (TextView) findViewById(R.id.TextViewLevelNo);
+		levelTxt.setText("Level: "+(++level));
+		GenerateRandomValues(gameLevel++);
+		TilesColorChange();
+	}
+
 	public void GenerateRandomValues(int amount) {
 
 		// amount=4;
+		try{
 		randomNumbers.removeAll(randomNumbers);
+		checkCount=0;
 		Random rand = new Random();
 		for (int generateCount = 0; generateCount < amount; generateCount++) {
 
-			randomNumbers.add(rand.nextInt((16 - 1) + 1) + 1);
+			randomNumbers.add(rand.nextInt((15 - 0) + 1) + 0);
+		}}
+		catch(Exception e){
+			Log.d("Error: ", e.toString());
 		}
 	}
-	
-	/* 1st attempt
-	public void color(View v, int count){
+
+	public void TilesColorChange() {
+		try{
+			int value = 0;		
+		int time = 1;
 		
+		
+		for (int count = 0; count < randomNumbers.size(); count++)
+		{
+			value = (Integer) randomNumbers.get(count);	
+			Log.d("Value: ",Integer.toString(value));
+			colorChange(value, time);
+			time=time+2;
+		}}
+	catch(Exception e){
+		Log.d("Error: ", e.toString());
+	}
+
+	}
+	public void colorChange(int value, int time){
+		try{
 	    final ImageView[] testImageView = new ImageView[1];
 	    
-	    switch (count){
-	      case 1: testImageView[0]=(ImageView)findViewById(R.id.ImageView01); break;
-	      case 2: testImageView[0]=(ImageView)findViewById(R.id.ImageView02); break;
-	      case 3: testImageView[0]=(ImageView)findViewById(R.id.ImageView03); break;
-	      case 4: testImageView[0]=(ImageView)findViewById(R.id.ImageView04); break;
-	      case 5: testImageView[0]=(ImageView)findViewById(R.id.ImageView05); break;
-	    
-	    }	    
-	    
-	     final Handler handler = new Handler(); 
-	     testImageView[0].setImageResource(R.drawable.img_clicked_tile);
-	     handler.postDelayed(new Runnable() { 
-	             public void run() { 
-	               testImageView[0].setImageResource(R.drawable.img_tile); 
-	               handler.postDelayed(this, 2000);
-	             } 
-	        }, 2000); 
-	       
-	  }*/
-	
-	/* 2nd attempt
-	public void color(View v, int count){
-	    final ImageView[] testImageView = new ImageView[1];
-	    
-	    switch (count){
-	      case 1: testImageView[0]=(ImageView)findViewById(R.id.ImageView01); break;
-	      case 2: testImageView[0]=(ImageView)findViewById(R.id.ImageView02); break;
-	      case 3: testImageView[0]=(ImageView)findViewById(R.id.ImageView03); break;
-	      case 4: testImageView[0]=(ImageView)findViewById(R.id.ImageView04); break;
-	      case 5: testImageView[0]=(ImageView)findViewById(R.id.ImageView05); break;	    
-	    }
-	    
-	    testImageView[0].setImageResource(R.drawable.img_clicked_tile);
-	    long start=System.currentTimeMillis();
-
-	    long end=start;
-	    while(true){
-	      if(end-start>2000){
-	        break;
-	      }
-	      end=System.currentTimeMillis(); 
-	    }
-	    testImageView[0].setImageResource(R.drawable.img_tile);
-	    
-	  }*/
-
-	/*3rd attempt
-	public void color(View v, int count){
-	    final ImageView[] testImageView = new ImageView[1];
-	    
-	    switch (count){
-	      case 1: testImageView[0]=(ImageView)findViewById(R.id.ImageView01); break;
-	      case 2: testImageView[0]=(ImageView)findViewById(R.id.ImageView02); break;
-	      case 3: testImageView[0]=(ImageView)findViewById(R.id.ImageView03); break;
-	      case 4: testImageView[0]=(ImageView)findViewById(R.id.ImageView04); break;
-	      case 5: testImageView[0]=(ImageView)findViewById(R.id.ImageView05); break;	    
-	    }
-	    
-	    testImageView[0].setImageResource(R.drawable.img_clicked_tile);
-	    
-	     new CountDownTimer(2000, 1000) {
-
-	     public void onTick(long millisUntilFinished) {
-	         
-		}
-
-	     public void onFinish() {
-	         testImageView[0].setImageResource(R.drawable.img_tile);
-	     }
-	  }.start();
-	  
-	  }*/
-
-	public void color(View v, int count, int time){
-	    final ImageView[] testImageView = new ImageView[1];
-	    
-	    switch (count){
-	      case 1: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol2); break;
-	      case 2: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol3); break;
-	      case 3: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol4); break;
-	      case 4: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol5); break;
-	      case 5: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol6); break;	    
+	    switch (value+1){
+	      case 1: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol1); break;
+	      case 2: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol2); break;
+	      case 3: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol3); break;
+	      case 4: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol4); break;
+	      case 5: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol5); break;
+	      case 6: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol6); break;
+	      case 7: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol7); break;
+	      case 8: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol8); break;
+	      case 9: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol9); break;
+	      case 10: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol10); break;
+	      case 11: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol11); break;
+	      case 12: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol12); break;
+	      case 13: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol13); break;
+	      case 14: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol14); break;
+	      case 15: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol15); break;
+	      case 16: testImageView[0]=(ImageView)findViewById(R.id.imageViewCol16); break;
+	      
 	    }  
 	  
 	     Handler handler = new Handler(); 	    
@@ -212,119 +157,13 @@ public class MemoryTiles extends ActionBarActivity {
 	             public void run() { 
 	               testImageView[0].setImageResource(R.drawable.img_tile);
 	             } 
-	        }, 1000*(time+1)); 
+	        }, 1000*(time+1)); }
+	catch(Exception e){
+		Log.d("Error: ", e.toString());
+	}
 	  }
 		   
-		    
-		/*final ImageView testImageView;
-		testImageView=(ImageView)findViewById(R.id.ImageView01);
-		
-		Thread t = new Thread() {
-		    public void run() {
-		      for(int i = 0; i < 5; i++)
-		      {
-		        try {
-		        	Log.d("-------------", "started");
-		        	testImageView.setImageResource(R.drawable.img_start);
-		          sleep(5000);
-		          testImageView.setImageResource(R.drawable.img_clicked_tile);
-		          
-		          
-		        } catch (Exception e) {
-		          Log.v("Error: ", e.toString());
-	}
-		      }
-		    }
-		  };
-		  t.start();*/
-		
-		/*for(int i = 0; i<5; i++){				
-			
-			testImageView=(ImageView)findViewById(R.id.ImageView01);
-			
-
-			Thread t = new Thread();
-			t.start();
-			testImageView.setImageResource(R.drawable.img_start);
-			Log.d("-------------", "started");
-			try{
-				t.sleep(2000);
-			}catch(Exception e)
-			{
-				Log.d("----------", "Error "+e.getMessage());
-			}
-			Log.d("-------------", "Second"+i);		
-			testImageView.setImageResource(R.drawable.img_sounds);
-			testImageView.setImageResource(R.drawable.img_clicked_tile);
-			
-		}*/
-	//}
-
-	public void TilesColorChange() {
-		
-		int value = 0;
-		long start;
-		long now;
-		Handler handler = new Handler(); 
-		for (int count = 0; count < randomNumbers.size(); count++)
-		{
-			value = (Integer) randomNumbers.get(count);	
-			//final int valuetemp=value;
-		/*	gridview.performItemClick(
-					gridview.getAdapter().getView(value, null, null), 
-					value,
-					gridview.getAdapter().getItemId(value));
-			*/
-			
-			//Change color, wait, change color back*/
-			
-			
-			LoadTileIcons.mThumbIds[value]=R.drawable.img_start;
-			
-			start = System.currentTimeMillis();
-		
-			while(true){
-				
-				now = System.currentTimeMillis();
-				if((now - start)>=2000){
-					break;
-				}
-				
-			}
-		   /* handler.postDelayed(new Runnable() { 
-			 
-		         public void run() { 
-		        	
-		        	 LoadTileIcons.mThumbIds[valuetemp]=R.drawable.img_tile;  
-		         } 
-		    }, 2000); */
-			
-			/*new CountDownTimer(2000, 1000) {
-
-			      public void onFinish() {
-			    	  LoadTileIcons.mThumbIds[valuetemp]=R.drawable.img_tile;  
-			     }
-
-				@Override
-				public void onTick(long arg0) {
-					// TODO Auto-generated method stub
-					
-				}
-			  }.start();*/
-		    
-			
-			/*try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			*/
-			LoadTileIcons.mThumbIds[value]=R.drawable.img_tile;  
-			
-		}
-
-	}
+	
 
 	private boolean CheckUserInput(int position) {
 		AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
@@ -332,7 +171,7 @@ public class MemoryTiles extends ActionBarActivity {
 		if (checkCount < randomNumbers.size()) {
 			if (position != (Integer) randomNumbers.get(checkCount)) {
 				alertDialog.setTitle("Game Over");
-				alertDialog.setMessage("Sorry, You got it wrong! Try Agian!");
+				alertDialog.setMessage("Sorry, You got it wrong! Try Agian! Your score is "+score+"!!!");
 				alertDialog.setIcon(R.drawable.logo);
 				alertDialog.setPositiveButton("OK",
 						new DialogInterface.OnClickListener() {
@@ -346,10 +185,13 @@ public class MemoryTiles extends ActionBarActivity {
 				alertDialog.show();
 
 			} else {
+				final TextView changeScore = (TextView) findViewById(R.id.textViewScore);
+				changeScore.setText("Score: "+(++score));
 				if (checkCount == randomNumbers.size() - 1) {
 					Toast.makeText(MemoryTiles.this,
 							"Congradulations! Next Level!!!",
 							Toast.LENGTH_SHORT).show();
+
 					return true;
 										
 				}
@@ -359,13 +201,86 @@ public class MemoryTiles extends ActionBarActivity {
 		return false;
 	}
 
-	public void GamePlay() {
-		checkCount=0;
-		test=false;
-		GenerateRandomValues(gameLevel++);
-		//TilesColorChange();
-		//color();
-		test = true;
+	public void imageViewCol1Clicked(View v)
+	{
+		Log.d("---------", "clicked 1");
+		CheckUserInput(0);
+		
 	}
-
+	public void imageViewCol2Clicked(View v)
+	{
+		Log.d("---------", "clicked 2");
+		CheckUserInput(1);
+	}
+	public void imageViewCol3Clicked(View v)
+	{
+		Log.d("---------", "clicked 3");
+		CheckUserInput(2);
+	}
+	public void imageViewCol4Clicked(View v)
+	{
+		Log.d("---------", "clicked 4");
+		CheckUserInput(3);
+	}
+	public void imageViewCol5Clicked(View v)
+	{
+		Log.d("---------", "clicked 5");
+		CheckUserInput(4);
+	}
+	public void imageViewCol6Clicked(View v)
+	{
+		Log.d("---------", "clicked 6");
+		CheckUserInput(5);
+	}
+	public void imageViewCol7Clicked(View v)
+	{
+		Log.d("---------", "clicked 7");
+		CheckUserInput(6);
+	}
+	public void imageViewCol8Clicked(View v)
+	{
+		Log.d("---------", "clicked 8");
+		CheckUserInput(7);
+	}
+	public void imageViewCol9Clicked(View v)
+	{
+		Log.d("---------", "clicked 9");
+		CheckUserInput(8);
+	}
+	public void imageViewCol10Clicked(View v)
+	{
+		Log.d("---------", "clicked 10");
+		CheckUserInput(9);
+	}
+	public void imageViewCol11Clicked(View v)
+	{
+		Log.d("---------", "clicked 11");
+		CheckUserInput(10);
+	}
+	public void imageViewCol12Clicked(View v)
+	{
+		Log.d("---------", "clicked 12");
+		CheckUserInput(11);
+	}
+	public void imageViewCol13Clicked(View v)
+	{
+		Log.d("---------", "clicked 13");
+		CheckUserInput(12);
+	}
+	public void imageViewCol14Clicked(View v)
+	{
+		Log.d("---------", "clicked 14");
+		CheckUserInput(13);
+	}
+	public void imageViewCol15Clicked(View v)
+	{
+		Log.d("---------", "clicked 15");
+		CheckUserInput(14);
+	}
+	public void imageViewCol16Clicked(View v)
+	{
+		Log.d("---------", "clicked 16");
+		CheckUserInput(15);
+	}
+	
 }
