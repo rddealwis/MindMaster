2a3d99aa347d1f87cca5141f36374e7e6d812e32
rddealwis <rddealwis@gmail.com>
2015-04-22 23:23:49 +0530

    Implemented the button click sounds

--- MindMaster/src/com/infinity/mindmaster/MemoryTiles.java
+++ MindMaster/src/com/infinity/mindmaster/MemoryTiles.java
@@ -1,22 +1,18 @@
 package com.infinity.mindmaster;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Random;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.InputBinding;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -39,11 +35,15 @@ public class MemoryTiles extends ActionBarActivity {
 	GridView gridview = null;
 	Handler handlerClick = new Handler();
 	final ImageView[] testImageViewClick = new ImageView[1];
+	MediaPlayer buttonSound = null;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_memory_tiles);
 		gridview = (GridView) findViewById(R.id.gridView_Tile);
+		buttonSound = MediaPlayer.create(MemoryTiles.this, R.raw.button_click);
 			
 	}
 
@@ -202,87 +202,105 @@ public class MemoryTiles extends ActionBarActivity {
 		checkCount++;
 		return false;
 	}
+	
+	
 
 	public void imageViewCol1Clicked(View v)
 	{
 		Log.d("---------", "clicked 1");
 		CheckUserInput(0);
+		buttonSound.start();
 		
 	}
 	public void imageViewCol2Clicked(View v)
 	{
 		Log.d("---------", "clicked 2");
 		CheckUserInput(1);
+		buttonSound.start();
 	}
 	public void imageViewCol3Clicked(View v)
 	{
 		Log.d("---------", "clicked 3");
 		CheckUserInput(2);
+		buttonSound.start();
 	}
 	public void imageViewCol4Clicked(View v)
 	{
 		Log.d("---------", "clicked 4");
 		CheckUserInput(3);
+		buttonSound.start();
 	}
 	public void imageViewCol5Clicked(View v)
 	{
 		Log.d("---------", "clicked 5");
 		CheckUserInput(4);
+		buttonSound.start();
 	}
 	public void imageViewCol6Clicked(View v)
 	{
 		Log.d("---------", "clicked 6");
 		CheckUserInput(5);
+		buttonSound.start();
 	}
 	public void imageViewCol7Clicked(View v)
 	{
 		Log.d("---------", "clicked 7");
 		CheckUserInput(6);
+		buttonSound.start();
 	}
 	public void imageViewCol8Clicked(View v)
 	{
 		Log.d("---------", "clicked 8");
 		CheckUserInput(7);
+		buttonSound.start();
 	}
 	public void imageViewCol9Clicked(View v)
 	{
 		Log.d("---------", "clicked 9");
 		CheckUserInput(8);
+		buttonSound.start();
 	}
 	public void imageViewCol10Clicked(View v)
 	{
 		Log.d("---------", "clicked 10");
 		CheckUserInput(9);
+		buttonSound.start();
 	}
 	public void imageViewCol11Clicked(View v)
 	{
 		Log.d("---------", "clicked 11");
 		CheckUserInput(10);
+		buttonSound.start();
 	}
 	public void imageViewCol12Clicked(View v)
 	{
 		Log.d("---------", "clicked 12");
 		CheckUserInput(11);
+		buttonSound.start();
 	}
 	public void imageViewCol13Clicked(View v)
 	{
 		Log.d("---------", "clicked 13");
 		CheckUserInput(12);
+		buttonSound.start();
 	}
 	public void imageViewCol14Clicked(View v)
 	{
 		Log.d("---------", "clicked 14");
 		CheckUserInput(13);
+		buttonSound.start();
 	}
 	public void imageViewCol15Clicked(View v)
 	{
 		Log.d("---------", "clicked 15");
 		CheckUserInput(14);
+		buttonSound.start();
 	}
 	public void imageViewCol16Clicked(View v)
 	{
 		Log.d("---------", "clicked 16");
 		CheckUserInput(15);
+		buttonSound.start();
 	}
 	
 }
